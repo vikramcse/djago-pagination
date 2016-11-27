f8db49ff84564697a98c70ad707b54cd90981e35
Vikram Jadhav <vikramcse.10@gmail.com>
2016-11-27 16:10:19 +0530

    pagination using django

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from api import views
 
 urlpatterns = [
     url(r'^getlist/', views.getlist),
-    url(r'^$', views.index)
+    url(r'^getlistwithdjangopagination/', views.get_list_with_django_pagination),
+    url(r'^$', views.index),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from api.models import Customer
 
@@ -25,3 +27,36 @@ def getlist(request):
         data.append(single_customer)
 
     return JsonResponse({'data': data})
+
+
+def get_list_with_django_pagination(request):
+    data = []
+    page_index = request.GET.get('pageIndex', None)
+    page_size = 10
+    customer_object = Customer.objects.all()
+
+    if page_index is not None:
+        paginator_object = Paginator(customer_object, page_size)
+
+        try:
+            obj = paginator_object.page(page_index)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            obj = paginator_object.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            obj = paginator_object.page(paginator_object.num_pages)
+
+        for customer in obj:
+            single_customer = {
+                'id': customer.id,
+                'first_name': customer.first_name,
+                'last_name': customer.last_name,
+                'email': customer.email,
+                'gender': customer.gender,
+                'ip_address': customer.ip_address
+            }
+
+            data.append(single_customer)
+
+        return JsonResponse({'data': data, 'count': customer_object.count()})
